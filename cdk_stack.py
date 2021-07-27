0787261bbe0715df08bce4ab289aaf6fc4af1c18
alexblb <87692886+alexblb@users.noreply.github.com>
2021-07-27 13:45:36 +0300

    Add files via upload

---
+++ cdk_stack.py
@@ -0,0 +1,16 @@
+from aws_cdk import (
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_s3 as s3,
+    core
+)
+
+
+class CdkStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self, "cdk-bucket")
